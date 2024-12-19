3794199a26a4920f06a39f5db89a70f3f20b3091
Luigi-44 <luigi.rondeau@gmail.com>
2024-12-19 14:37:38 +0100

    Header V1

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,17 +1,17 @@
 import "./Header.css"
-import logo from "../assets/react.svg";
+import logo from "../assets/Logo.png";
 
 function Header() {
     return (
-    <div className="divHeader">
-        <nav className="navHeader">
-            <ul className="ulHeader">
-                <li>Meet</li>
-                <li><img src={logo}alt="logo"/></li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    </div> 
+        <div className="divHeader">
+            <nav className="navHeader">
+                <ul className="ulHeader">
+                    <li>Meet</li>
+                    <li><img src={logo}alt="logo"/></li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div> 
     )
 }
 export default Header
