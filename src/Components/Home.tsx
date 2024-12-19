2e6889922396527a256ae7da5d67a1a03feeec35
luciemiszewski <miszewskilucie@outlook.com>
2024-12-19 14:49:20 +0100

    Planètes

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,5 +1,54 @@
+import { useEffect, useState } from "react";
+import "./Home.css";
+import pngegg from "../assets/pngegg.png";
+import William from "../assets/William.png";
+import Lucie from "../assets/Lucie.png";
+import Kashyyyk_Planet_Preview from "../assets/Kashyyyk_Planet_Preview.png";
+
 function Home() {
-    return <div>Test</div>
+    const [planetes, setPlanetes] = useState([]);
+
+    useEffect(() => {
+        fetch("https://starwarsapi.remote-8.wilders.dev/api/homeworld?take=4")
+            .then((res) => res.json())
+            .then((data) => {
+                if (Array.isArray(data)) {
+                    setPlanetes(data);
+                }
+            })
+            .catch((err) =>
+                console.error("Erreur lors de la récupération des données :", err)
+            );
+    }, []);
+
+    return (
+        <main>
+            <h1>Planètes</h1>
+            <div className="planete-container">
+                {planetes.map((planete, index) => (
+                    <div key={planete.name} className="planete-card">
+                        <div className="test">
+                            <p className="planete-name">{planete.name}</p>
+                        </div>
+                        {/* Alternance des images en fonction de l'index */}
+                        <img
+                            className="planete-image"
+                            src={
+                                index % 4 === 0
+                                    ? pngegg
+                                    : index % 4 === 1
+                                        ? William
+                                        : index % 4 === 2
+                                            ? Lucie
+                                            : Kashyyyk_Planet_Preview
+                            }
+                            alt={`Planète ${planete.name}`}
+                        />
+                    </div>
+                ))}
+            </div>
+        </main>
+    );
 }
 
 export default Home;
