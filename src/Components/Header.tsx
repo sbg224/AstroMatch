8b0bdfbc6aa09250c2ad5bf2ece0ea0baf4180f0
Luigi-44 <luigi.rondeau@gmail.com>
2024-12-19 17:24:54 +0100

    lien des pages MEET et CONTACT fais sur le header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,14 +1,15 @@
 import "./Header.css"
 import logo from "../assets/Logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className="divHeader">
+        <div id="divHeader">
             <nav className="navHeader">
                 <ul className="ulHeader">
-                    <li>Meet</li>
+                    <li><Link to="/meet">Meet</Link></li>
                     <li><img src={logo}alt="logo"/></li>
-                    <li>Contact</li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </div> 
