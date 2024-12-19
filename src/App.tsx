20228c46f5f97a95111f9606c75a3e1772983df9
luciemiszewski <miszewskilucie@outlook.com>
2024-12-19 11:18:03 +0100

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Home from "./Components/Home";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
 	return (
 		<>
 			<Outlet/>
+			<Home />
 		</>
 	);
 }
